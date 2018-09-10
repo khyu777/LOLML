f4e22e2d083ac0af87e0ed52dbc89ae6437e0577
David <khyu777@gmail.com>
2018-09-10 01:11:41 -0400

    add option for scaling in model

--- model.py
+++ model.py
@@ -1,21 +1,29 @@
 import pandas as pd
 import os
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 
-filename = 'LCK Summer 2017'
+filename = 'LCK Summer 2018'
 
-regular = pd.read_csv('Data/LCK Summer 2017.csv', index_col=0)
-playoff = pd.read_csv('Data/LCK Summer 2017 Playoffs.csv', index_col=0)
+regular = pd.read_csv('Data/' + filename + '.csv', index_col=0)
+playoff = pd.read_csv('Data/' + filename + ' Playoffs.csv', index_col=0)
 
 train_X = regular.iloc[:, 3:-1]
 train_Y = regular.iloc[:, -1]
 test_X = playoff.iloc[:, 3:-1]
 test_Y = playoff.iloc[:, -1]
+'''
+min_max_scaler = preprocessing.MinMaxScaler()
+np_scaled = min_max_scaler.fit_transform(train_X)
+train_X = pd.DataFrame(np_scaled)
 print(train_X)
 
+np_test_scaled = min_max_scaler.fit_transform(test_X)
+test_X = pd.DataFrame(np_test_scaled)
+print(test_X)
+'''
 lm = linear_model.LinearRegression()
 lm.fit(train_X, train_Y)
 y_pred = lm.predict(test_X)
@@ -31,4 +39,4 @@ print('Variance score: %.2f' % r2_score(test_Y, y_pred))
 test_Y = pd.DataFrame(test_Y)
 playoff['prediction'] = y_pred
 print(playoff.iloc[:, 3:])
-playoff.to_csv('C:/Users/khyu7/Documents/Coding/Lol/Data/' + filename + '_pred.csv')
+#playoff.to_csv('C:/Users/khyu7/Documents/Coding/Lol/Data/' + filename + '_pred.csv')
